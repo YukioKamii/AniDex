c95cbf52811e8e319e7f6a4b6f4a32ec8452ec9e
Mathistrg <mathis.truong@next-u.fr>
2026-03-25 13:25:58 +0100

    chore: configure Apollo GraphQL in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.apollographql.apollo") version "4.4.1"
 }
 
 android {
     namespace = "com.example.anidex"
+
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -30,10 +32,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         compose = true
     }
@@ -43,12 +47,16 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material:material-icons-extended")
+
+    implementation("com.apollographql.apollo:apollo-runtime:4.4.1")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +64,10 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
+
+apollo {
+    service("anilist") {
+        packageName.set("com.example.anidex")
+    }
 }
